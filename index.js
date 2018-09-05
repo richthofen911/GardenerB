b363c847605a41660ff3bfbf8afa6b251a1fa56f
yichao li <yichaoli.richthofe@gmail.com>
2018-09-05 11:24:54 -0400

    .

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg')
@@ -8,34 +7,24 @@ const pool = new Pool({
   ssl: true
 })
 
-/*
-app.get('/db', async(req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECt * FROM test_table')
-    res.render('pages/db', result);
-    client.release();
-  } catch(err) {
-    console.error(err)
-    res.send("Error " + err)
-  }
-})
-*/
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/db', async(req, res) => {
+    res.json({a:1});
+    
     try {
       const client = await pool.connect()
       const result = await client.query('SELECt * FROM test_table')
-      res.render('pages/db', result);
+      res.json({record: result});
       client.release();
     } catch(err) {
       console.error(err)
       res.send("Error " + err)
     }
-  })
+  }
+)
+  
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
