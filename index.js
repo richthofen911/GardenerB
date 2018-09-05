236b7023b5e084b967b4e527fc825d5f28e0c027
yichao li <yichaoli.richthofe@gmail.com>
2018-09-05 11:16:28 -0400

    Add row to db.

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 const express = require('express')
+const app = express();
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const { Pool } = require('pg')
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+})
+
+/*
+app.get('/db', async(req, res) => {
+  try {
+    const client = await pool.connect()
+    const result = await client.query('SELECt * FROM test_table')
+    res.render('pages/db', result);
+    client.release();
+  } catch(err) {
+    console.error(err)
+    res.send("Error " + err)
+  }
+})
+*/
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async(req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECt * FROM test_table')
+      res.render('pages/db', result);
+      client.release();
+    } catch(err) {
+      console.error(err)
+      res.send("Error " + err)
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
