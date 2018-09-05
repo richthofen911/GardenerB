9b83192a0639cc1682045b79b7df9d2ac4ad93f1
yichao li <yichaoli.richthofe@gmail.com>
2018-09-05 12:38:36 -0400

    .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const multer = require('multer')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const { Pool } = require('pg')
@@ -9,20 +10,30 @@ const pool = new Pool({
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(multer().array()) // to parse form-data
+  .use(express.urlencoded({ extended: false })) // to parse form-url-encoded
+  .use(express.json()) // to parse application/json
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async(req, res) => {
+  .post('/login', async(req, res) => {
     try {
+      //res.json({requestBody: req.body, requestHeader: req.headers})
+      const id = req.body.id
+      const name = req.body.name
+      const passwd = req.body.passwd
+    
       const client = await pool.connect()
-      const result = await client.query('SELECt * FROM test_table')
+      const queryStr = `INSERT INTO users values (${id}, ${name}, ${passwd})`
+      //res.send(queryStr)
+      await client.query(queryStr)
+      const result = await client.query('SELECt * FROM users')
       res.json({record: result});
       client.release();
+      
     } catch(err) {
       console.error(err)
       res.send("Error " + err)
     }
-  }
-)
-  
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
